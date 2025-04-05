d8a08cb7b74cee5f881bd073e0f3b01583477f1b
SangHyun Kim <138132335+ddanghyni@users.noreply.github.com>
2025-04-05 23:34:08 +0900

    Title: 점수 비교, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250405/점수 비교/score-comparison.py
+++ 250405/점수 비교/score-comparison.py
@@ -2,7 +2,7 @@ A_math, A_eng = map(int, input().split())
 
 B_math, B_eng = map(int, input().split())
 
-if (A_math > B_math) & (A_math > B_math):
+if (A_math > B_math) & (A_eng > B_eng):
     print(1)
 else:
     print(0)
