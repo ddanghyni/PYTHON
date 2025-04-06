132a8246e52d5d29c8b24e951cbad70dd12043ac
SangHyun Kim <138132335+ddanghyni@users.noreply.github.com>
2025-04-06 12:31:57 +0900

    Title: 1부터의 합, Time: 57ms, Memory: 16MB, Status: Passed - Codetree

--- 250406/1부터의 합/sum-from-the-one.py
+++ 250406/1부터의 합/sum-from-the-one.py
@@ -3,6 +3,6 @@ n = int(input())
 arr = list(range(1, 101))
 
 for i in range(len(arr)):
-    if sum(arr[1:i+2]) >= n:
-        print(arr[i+1])
+    if sum(arr[0:i+1]) >= n:
+        print(arr[i])
         break
