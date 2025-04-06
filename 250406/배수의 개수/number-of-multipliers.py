59e6f25939313a658593066ab63c8872f467bcaf
SangHyun Kim <138132335+ddanghyni@users.noreply.github.com>
2025-04-06 12:45:30 +0900

    Title: 배수의 개수, Time: 71ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250406/배수의 개수/number-of-multipliers.py
+++ 250406/배수의 개수/number-of-multipliers.py
@@ -4,13 +4,9 @@ times_3 = 0
 times_5 = 0
 
 for i in arr:
-    if i % 15 == 0:
+    if i % 3 == 0:
         times_3 += 1
+    if i % 5 == 0:
         times_5 += 1
-    elif i % 5 == 0:
-        times_5 += 1
-    elif i % 3 == 0:
-        times_3 += 1
-    else:
-        None
-print(times_3, times_5)
+
+print(times_3, times_5
