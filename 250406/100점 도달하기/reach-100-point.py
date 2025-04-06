99b6c8b2ff270fd65abda7bc3b99da0dbcb85162
SangHyun Kim <138132335+ddanghyni@users.noreply.github.com>
2025-04-06 11:58:23 +0900

    Title: 100점 도달하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250406/100점 도달하기/reach-100-point.py
@@ -0,0 +1,13 @@
+score = int(input())
+
+for x in range(score, 101, 1):
+    if x >= 90:
+        print("A", end = " ")
+    elif x >= 80:
+        print("B", end = " ")
+    elif x >= 70:
+        print("C", end = " ")
+    elif x >= 60:
+        print("D", end = " ")
+    else:
+        print("F")
